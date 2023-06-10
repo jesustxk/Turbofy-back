faaef6f0470185201c0e172adca41fa5cf68eddb
Jesús Checa <jesuscheca11@gmail.com>
2023-06-10 23:51:12 +0200

    feat: Spoty controller

---
+++ code/controllers/spotyController.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+
+const sendJSONresponse = (res, status, content) => {
+    res.status(status);
+    res.json(content);
+};
+
+// GET /songs/spoty/read
+const readSpotySong = async (req, res) => {
+    try {
+        const songs = getSongs(req, res);
+        sendJSONresponse(res, 200, songs);
+    } catch (error) {
+
+        if (error.response.status === 401) {
+            console.log('Token invalido, se solicita uno nuevo y se reintenta la llamada a la API')
+            updateToken();
+        } else {
+            sendJSONresponse(res, error.response.status, { 'message': 'Error al tratar de obtener el listado de canciones, inténtelo de nuevo' });
+        }
+    }
+};
+
+const getSongs = async (req, res) => {
+    const songs = await fetch('https://api.spotify.com/v1/search?q=' + req.params.searchParams + '&type=track&limit=10', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + process.env.SPOTY_TOKEN,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+    res.json(songs);
+}
+
+const updateToken = async (req, res) => {
+    const bearerToken = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            'grant_type': 'client_credentials',
+            'client_id': process.env.SPOTY_ID,
+            'client_secret': process.env.SPOTY_SECRET
+        })
+    })
+
+    console.log('Nuevo bearer token obtenido');
+
+    // Actualizamos el token
+    process.env.SPOTY_TOKEN = bearerToken.access_token;
+
+    // Recuperamos las canciones
+    try {
+        const songs = getSongs(req, res);
+        sendJSONresponse(res, 200, songs);
+    } catch (error) {
+        sendJSONresponse(res, error.response.status, { 'message': 'Error al tratar de obtener el listado de canciones, inténtelo de nuevo' });
+    }
+};
+
+module.exports = {
+    readSpotySong
+};
--- code/routes/songsRoutes.js
+++ code/routes/songsRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const songsController = require('../controllers/songsController');
+const { default: spotyController } = require('../controllers/spotyController');
 
 // SONGS
 router.post('/songs/create', songsController.createSong);
@@ -15,5 +16,8 @@ router.get('/comments/read', songsController.readComment);
 router.get('/comments/readSong', songsController.readSongComments);
 router.post('/comments/update', songsController.updateComment);
 router.delete('/comments/delete', songsController.deleteComment);
+
+// SEARCH SONGS SPOTIFY
+router.get('/songs/spoty/read', spotyController.readSpotySong);
 
 module.exports = router;
