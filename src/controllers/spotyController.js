11397f488aa25da97ba878ae1683f1a05a0fb984
Jesús Checa <jesuscheca11@gmail.com>
2023-06-13 01:05:47 +0200

    feat: SpotyController almost working (cant find artist-track)

--- src/controllers/spotyController.js
+++ src/controllers/spotyController.js
@@ -7,31 +7,32 @@ const sendJSONresponse = (res, status, content) => {
 
 // GET /songs/spoty/read
 const readSpotySong = async (req, res) => {
-    try {
-        const songs = await getSongs(req, res);
-        sendJSONresponse(res, 200, songs);
-    } catch (error) {
+    if (process.env.SPOTY_TOKEN === '') {
+        await updateToken(req, res);
+    } else {
+        try {
+            const songsResponse = await fetch('https://api.spotify.com/v1/search?type=track&q=' + req.query.searchParams + '&limit=10', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + process.env.SPOTY_TOKEN,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
 
-        if (error.response.status === 401) {
-            console.log('Token invalido, se solicita uno nuevo y se reintenta la llamada a la API')
-            await updateToken();
-        } else {
-            sendJSONresponse(res, error.response.status, { 'message': 'Error al tratar de obtener el listado de canciones, inténtelo de nuevo' });
+            const songs = await songsResponse.json();
+            console.log(songs)
+
+            sendJSONresponse(res, 200, songs);
+        } catch (err) {
+            if (err.response.status === 401) {
+                console.log('Token invalido, se solicita uno nuevo y se reintenta la llamada a la API')
+                await updateToken();
+            } else {
+                sendJSONresponse(res, 404, { 'message': 'Error al tratar de obtener el listado de canciones, inténtelo de nuevo' });
+            }
         }
     }
 };
-
-const getSongs = async (req, res) => {
-    const songs = await fetch('https://api.spotify.com/v1/search?q=' + req.params.searchParams + '&type=track&limit=10', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + process.env.SPOTY_TOKEN,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    });
-
-    res.json(songs);
-}
 
 const updateToken = async (req, res) => {
     const bearerToken = await fetch('https://accounts.spotify.com/api/token', {
@@ -46,17 +47,32 @@ const updateToken = async (req, res) => {
         })
     })
 
+    const token = await bearerToken.json();
+
     console.log('Nuevo bearer token obtenido');
 
     // Actualizamos el token
-    process.env.SPOTY_TOKEN = bearerToken.access_token;
+    process.env.SPOTY_TOKEN = token.access_token;
+
+    console.log(process.env.SPOTY_TOKEN)
 
     // Recuperamos las canciones
     try {
-        const songs = await getSongs(req, res);
+        console.log('https://api.spotify.com/v1/search?type=track&q=' + req.query.searchParams + '&limit=10')
+        const songsResponse = await fetch('https://api.spotify.com/v1/search?type=track&q=' + req.query.searchParams + '&limit=10', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + process.env.SPOTY_TOKEN,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const songs = await songsResponse.json();
+        console.log(songs)
+
         sendJSONresponse(res, 200, songs);
     } catch (error) {
-        sendJSONresponse(res, error.response.status, { 'message': 'Error al tratar de obtener el listado de canciones, inténtelo de nuevo' });
+        sendJSONresponse(res, 404, { 'message': 'Error al tratar de obtener el listado de canciones, inténtelo de nuevo' });
     }
 };
 
